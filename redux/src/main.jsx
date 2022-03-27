df7cfcdeb588397f3eb2a1cf23f071954fa93608
yashimwong <yashimwong@gmail.com>
2022-03-27 23:45:18 +0800

    feat: add flag for redux dev tools

--- redux/src/main.jsx
+++ redux/src/main.jsx
@@ -6,7 +6,7 @@ import rootReducer from "./reducers";
 import "./index.css";
 import App from "./App";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <React.StrictMode>
